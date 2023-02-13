90b6141e274769a35aa55e655d5c43dad9ec5b96
Adam Belakovics <beladi96@gmail.com>
2023-02-13 12:59:32 +0100

    Add part 1, 2 and 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { useState } from "react";
+import { Form } from "./Form";
+import { Grid } from "./Grid";
+import { Result } from "./Result";
+
 function App() {
-  return <div>Hello</div>;
+  const [stage, setStage] = useState(1);
+
+  const [config, setConfig] = useState(null);
+
+  const [stepsArray, setStepsArray] = useState([]);
+
+  if (stage === 1) {
+    return (
+      <Form
+        handleNext={(values) => {
+          setConfig(values);
+          setStage(2);
+        }}
+      />
+    );
+  }
+
+  if (stage === 2) {
+    return (
+      <Grid
+        config={config}
+        currentNumberOfSteps={stepsArray.length}
+        setStepsArray={setStepsArray}
+        handleNext={() => setStage(3)}
+      />
+    );
+  }
+
+  if (stage === 3) {
+    return <Result stepsArray={stepsArray} />;
+  }
+
+  console.error("Invalid stage number");
+
+  return <Form />;
 }
 
 export default App;
